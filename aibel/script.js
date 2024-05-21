3090137a2a99d0f518bbd4c78c5c3632f0671ef9
aibel10 <150226390+aibel10@users.noreply.github.com>
2024-05-21 14:59:09 +0530

    Add files via upload

---
+++ aibel/script.js
@@ -0,0 +1,95 @@
+// postfix increment
+//  let a=6;
+// let b=a++;
+// console.log(a);
+// console.log(b);
+
+// prifix increment
+// let p=4;
+// let q=++p;
+// console.log("p",p);
+// console.log("q",q);
+
+// let car={
+//     company: "Toyota",
+//     model: "fortuner",
+//     color: "Black"
+// };
+// console.log(car.company);
+
+// let persons={
+//     fname:"aibel",
+//     adress:{
+//         street:"abc",
+//         place:"def",
+//         road:"kddhffgdjdgjdsg",
+//         pin:"35436"
+//     },
+//     age:"65",
+
+// };
+//  console.log(persons.fname);
+//  console.log(persons.adress);
+//  console.log(persons.age);
+
+// var umikari=new Object();
+// umikari.name="deepthi";
+// umikari.color="chomala";
+
+// console.log(umikari);
+
+// concatination
+
+// let fname="Rangan";
+// let laname="Ponnapan";
+// console.log(`${fname} and ${laname}`)
+
+// let m=5;
+// let n=6;
+// console.log(`the sum is $(m+n)`);
+
+// Array
+// let fruits=["Apple","Mango","Orange",23]
+// console.log(fruits);
+// fruits.push("Grapes");
+// console.log(fruits);
+// console.log(fruits.length);
+// fruits[2]="Pineapple";
+// console.log(fruits);
+// fruits.unshift("Pear");
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+
+// Array of Object
+// let bsc=[
+//     {
+//         fname:"Ronaldhino",
+//         age:50,
+//     },
+//     {
+//         fname:"Cafu",
+//         age:40,
+//     },
+
+//     {
+//         fname:"Beckam",
+//         age:45,
+//     },
+// ];
+// console.log(bsc[1].age);
+     
+// function add(a,b){
+//     let c=a+b;
+//     console.log(c);
+
+// }
+// add(1,2)
+function fullname(fname,lname){
+    console.log(`${fname} ${lname}`)
+};
+console.log("Rangan","Soman");
+ 
+
